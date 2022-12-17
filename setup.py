79509a29173b3bd180c7134b7c04acf1fb357cbc
Peter Reinboth <github@preinboth.de>
2022-12-17 05:00:23 +0100

    import

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ setup(
     name='EdApiConnector',
     version='v0.0.1',
     packages=find_packages(),
-    package_dir={'': 'src/EDApiConnector'},
+    package_dir={'': 'src'},
     url='https://github.com/preinboth/EdApiConnector',
     license='MIT',
     author='preinboth',
-    author_email='',
+    author_email='github@preinboth.de',
     description='Elite Dangerous Api Connector',
     install_requires=['requests']
 )
